f53c205e4efe40bf958f45a80c32ab8c0b3c1e25
Denis <dddenisss35@gmail.com>
2024-01-20 11:29:10 +0200

    AddressablesSourceGenerator

--- Assets/UnityEngine.AddressableAssets/R.cs
+++ Assets/UnityEngine.AddressableAssets/R.cs
@@ -1,5 +1,20 @@
 namespace UnityEngine.AddressableAssets {
     public static class @R {
+        public static class @Global {
+            public static class @Aaa {
+                public const string @Asset = "Global/Aaa/Asset.txt";
+                public const string @Asset_1 = "Global/Aaa/Asset 1.txt";
+                public const string @Asset_2 = "Global/Aaa/Asset 2.txt";
+            }
+            public static class @Bbb {
+                public const string @Asset = "Global/Bbb/Asset.txt";
+                public const string @Asset_1 = "Global/Bbb/Asset 1.txt";
+                public const string @Asset_2 = "Global/Bbb/Asset 2.txt";
+            }
+            public const string @Asset = "Global/Asset.txt";
+            public const string @Asset_1 = "Global/Asset 1.txt";
+            public const string @Asset_2 = "Global/Asset 2.txt";
+        }
         public static class @MyProject {
             public static class @Aaa {
                 public const string @Asset = "MyProject.Aaa/Asset.txt";
