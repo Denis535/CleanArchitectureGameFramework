e7859a377b035d5480230bb40d6dfb15a164a7af
Denis <dddenisss35@gmail.com>
2024-04-07 17:52:23 +0300

    PrefabHandle

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/PrefabHandle.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/PrefabHandle.cs
@@ -10,16 +10,16 @@ namespace UnityEngine.AddressableAssets {
     using UnityEngine.ResourceManagement.AsyncOperations;
     using UnityEngine.ResourceManagement.ResourceProviders;
 
-    public class PrefabHandle {
+    public class PrefabHandle<T> where T : notnull, Component {
 
-        private AsyncOperationHandle<GameObject> instance;
+        private AsyncOperationHandle<T> instance;
         private CancellationTokenRegistration destroyCancellationTokenRegistration;
 
         public string Key { get; }
         public bool IsValid => instance.IsValid();
         public bool IsSucceeded => instance.IsValid() && instance.IsSucceeded();
         public bool IsFailed => instance.IsValid() && instance.IsFailed();
-        public GameObject Instance {
+        public T Instance {
             get {
                 Assert.Operation.Message( $"PrefabHandle {this} must be valid" ).Valid( instance.IsValid() );
                 Assert.Operation.Message( $"PrefabHandle {this} must be succeeded" ).Valid( instance.IsSucceeded() );
@@ -33,20 +33,20 @@ namespace UnityEngine.AddressableAssets {
         }
 
         // InstantiateAsync
-        public Task<GameObject> InstantiateAsync(CancellationToken destroyCancellationToken) {
+        public Task<T> InstantiateAsync(CancellationToken destroyCancellationToken) {
             return InstantiateAsync( Vector3.zero, Quaternion.identity, null, destroyCancellationToken );
         }
-        public Task<GameObject> InstantiateAsync(Transform? parent, CancellationToken destroyCancellationToken) {
+        public Task<T> InstantiateAsync(Transform? parent, CancellationToken destroyCancellationToken) {
             return InstantiateAsync( Vector3.zero, Quaternion.identity, parent, destroyCancellationToken );
         }
-        public Task<GameObject> InstantiateAsync(Vector3 position, Quaternion rotation, Transform? parent, CancellationToken destroyCancellationToken) {
+        public Task<T> InstantiateAsync(Vector3 position, Quaternion rotation, Transform? parent, CancellationToken destroyCancellationToken) {
             Assert.Operation.Message( $"PrefabHandle {this} already exists" ).Valid( !instance.IsValid() );
             Assert.Operation.Message( $"PrefabHandle {this} already exists" ).Valid( destroyCancellationTokenRegistration == default );
-            instance = Addressables.InstantiateAsync( Key, new InstantiationParameters( position, rotation, parent ) );
+            instance = InstantiateAsync( Key, position, rotation, parent );
             destroyCancellationTokenRegistration = destroyCancellationToken.Register( () => Destroy() );
             return GetInstanceAsync( destroyCancellationToken );
         }
-        public async Task<GameObject> GetInstanceAsync(CancellationToken cancellationToken) {
+        public async Task<T> GetInstanceAsync(CancellationToken cancellationToken) {
             Assert.Operation.Message( $"PrefabHandle {this} must be valid" ).Valid( instance.IsValid() );
             if (instance.Status is AsyncOperationStatus.None or AsyncOperationStatus.Succeeded) {
                 var result = await instance.Task.WaitAsync( cancellationToken ).ConfigureAwait( false );
@@ -71,10 +71,23 @@ namespace UnityEngine.AddressableAssets {
             return Key;
         }
 
+        // Helpers
+        private static AsyncOperationHandle<T> InstantiateAsync(string key, Vector3 position, Quaternion rotation, Transform? parent) {
+            var instance = Addressables.InstantiateAsync( key, new InstantiationParameters( position, rotation, parent ) );
+            return Addressables.ResourceManager.CreateChainOperation<T, GameObject>( instance, i => {
+                var result = i.Result.GetComponent<T>();
+                if (result != null) {
+                    return Addressables.ResourceManager.CreateCompletedOperation( result, null );
+                } else {
+                    return Addressables.ResourceManager.CreateCompletedOperation( result!, $"Component '{typeof( T )}' was not found" );
+                }
+            } );
+        }
+
     }
     public static class PrefabHandleExtensions {
 
-        public static void DestroyAll<T>(this IEnumerable<PrefabHandle> collection) {
+        public static void DestroyAll<T>(this IEnumerable<PrefabHandle<T>> collection) where T : notnull, Component {
             foreach (var item in collection.Where( i => i.IsValid )) {
                 item.Destroy();
             }
