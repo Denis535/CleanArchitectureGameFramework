a69f0c38fc7334a66d76d67852873b8fcd2619eb
Denis <dddenisss35@gmail.com>
2024-04-18 14:56:54 +0300

    InstanceHandle

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/InstanceHandle.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/InstanceHandle.cs
@@ -8,34 +8,7 @@ namespace UnityEngine.AddressableAssets {
     using UnityEngine;
     using UnityEngine.ResourceManagement.AsyncOperations;
 
-    public interface IInstanceHandle<out T> where T : notnull, Component {
-
-        // State
-        bool IsValid { get; }
-        bool IsDone { get; }
-        bool IsSucceeded { get; }
-        bool IsFailed { get; }
-        // Value
-        T Value { get; }
-        T? ValueSafe { get; }
-        // Exception
-        Exception? Exception { get; }
-
-        // InstantiateAsync
-        //ValueTask<T> InstantiateAsync(CancellationToken cancellationToken);
-        //ValueTask<T> InstantiateAsync(Vector3 position, Quaternion rotation, CancellationToken cancellationToken);
-        //ValueTask<T> InstantiateAsync(Transform parent, CancellationToken cancellationToken);
-        //ValueTask<T> InstantiateAsync(Vector3 position, Quaternion rotation, Transform parent, CancellationToken cancellationToken);
-
-        // GetValueAsync
-        //ValueTask<T> GetValueAsync(CancellationToken cancellationToken);
-
-        // ReleaseInstance
-        void ReleaseInstance();
-        void ReleaseInstanceSafe();
-
-    }
-    public class InstanceHandle<T> : AddressableHandle<T>, IInstanceHandle<T>, ICloneable where T : notnull, Component {
+    public class InstanceHandle<T> : AddressableHandle<T>, ICloneable where T : notnull, Component {
 
         // Constructor
         public InstanceHandle(string key) : base( key ) {
@@ -83,13 +56,12 @@ namespace UnityEngine.AddressableAssets {
         }
         public InstanceHandle<T> Clone() {
             Assert_IsValid();
-            var clone = new InstanceHandle<T>( Key, Handle );
             Addressables.ResourceManager.Acquire( Handle );
-            return clone;
+            return new InstanceHandle<T>( Key, Handle );
         }
 
     }
-    public class DynamicInstanceHandle<T> : DynamicAddressableHandle<T>, IInstanceHandle<T>, ICloneable where T : notnull, Component {
+    public class DynamicInstanceHandle<T> : DynamicAddressableHandle<T>, ICloneable where T : notnull, Component {
 
         // Constructor
         public DynamicInstanceHandle() {
@@ -137,9 +109,8 @@ namespace UnityEngine.AddressableAssets {
         }
         public InstanceHandle<T> Clone() {
             Assert_IsValid();
-            var clone = new InstanceHandle<T>( Key, Handle );
             Addressables.ResourceManager.Acquire( Handle );
-            return clone;
+            return new InstanceHandle<T>( Key, Handle );
         }
 
     }
