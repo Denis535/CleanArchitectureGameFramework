abc091fd48051e5a423c5bb914861e04206a2bf6
Denis <dddenisss35@gmail.com>
2024-04-26 23:48:46 +0300

    Addressables2

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/AddressableHelper.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework/UnityEngine.AddressableAssets/AddressableHelper.cs
@@ -4,113 +4,58 @@ namespace UnityEngine.AddressableAssets {
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
     using UnityEngine;
     using UnityEngine.ResourceManagement.AsyncOperations;
 
     internal static class AddressableHelper {
-
-        // IsState
-        public static bool IsNone<T>(this AsyncOperationHandle<T> handle) {
-            return handle.Status == AsyncOperationStatus.None;
-        }
-        public static bool IsSucceeded<T>(this AsyncOperationHandle<T> handle) {
-            return handle.Status == AsyncOperationStatus.Succeeded;
-        }
-        public static bool IsFailed<T>(this AsyncOperationHandle<T> handle) {
-            return handle.Status == AsyncOperationStatus.Failed;
-        }
 
         // LoadAssetAsync
         public static AsyncOperationHandle<T> LoadAssetAsync<T>(string key) where T : UnityEngine.Object {
             return Addressables.LoadAssetAsync<T>( key );
         }
         public static AsyncOperationHandle<IReadOnlyList<T>> LoadAssetListAsync<T>(string[] keys) where T : UnityEngine.Object {
-            return Addressables.LoadAssetsAsync<T>( keys.AsEnumerable(), null, Addressables.MergeMode.Union ).ChainOperation( assets => {
-                return Addressables.ResourceManager.CreateCompletedOperation( (IReadOnlyList<T>) assets.Result, null );
+            return Addressables.LoadAssetsAsync<T>( keys.AsEnumerable(), null, Addressables.MergeMode.Union ).ChainOperation( assetsHandle => {
+                return Addressables.ResourceManager.CreateCompletedOperation( (IReadOnlyList<T>) assetsHandle.Result, null );
             } );
         }
 
         // LoadPrefabAsync
         public static AsyncOperationHandle<T> LoadPrefabAsync<T>(string key) where T : Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return RequireComponentOperation<T>( prefab.Result );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabHandle => {
+                return RequireComponentOperation<T>( prefabHandle.Result );
             } );
         }
         public static AsyncOperationHandle<IReadOnlyList<T>> LoadPrefabListAsync<T>(string[] keys) where T : Component {
-            return Addressables.LoadAssetsAsync<GameObject>( keys.AsEnumerable(), null, Addressables.MergeMode.Union ).ChainOperation( prefabs => {
-                return RequireComponentsOperation<T>( (IReadOnlyList<GameObject>) prefabs.Result );
+            return Addressables.LoadAssetsAsync<GameObject>( keys.AsEnumerable(), null, Addressables.MergeMode.Union ).ChainOperation( prefabsHandle => {
+                return RequireComponentsOperation<T>( (IReadOnlyList<GameObject>) prefabsHandle.Result );
             } );
         }
 
         // InstantiateAsync
         public static AsyncOperationHandle<T> InstantiateAsync<T>(string key) where T : notnull, Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return InstantiateOperation<T>( prefab.Result );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabsHandle => {
+                return InstantiateOperation<T>( prefabsHandle.Result );
             } );
         }
         public static AsyncOperationHandle<T> InstantiateAsync<T>(string key, Vector3 position, Quaternion rotation) where T : notnull, Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return InstantiateOperation<T>( prefab.Result, position, rotation );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabsHandle => {
+                return InstantiateOperation<T>( prefabsHandle.Result, position, rotation );
             } );
         }
         public static AsyncOperationHandle<T> InstantiateAsync<T>(string key, Transform? parent) where T : notnull, Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return InstantiateOperation<T>( prefab.Result, parent );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabsHandle => {
+                return InstantiateOperation<T>( prefabsHandle.Result, parent );
             } );
         }
         public static AsyncOperationHandle<T> InstantiateAsync<T>(string key, Vector3 position, Quaternion rotation, Transform? parent) where T : notnull, Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return InstantiateOperation<T>( prefab.Result, position, rotation, parent );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabsHandle => {
+                return InstantiateOperation<T>( prefabsHandle.Result, position, rotation, parent );
             } );
         }
         public static AsyncOperationHandle<T> InstantiateAsync<T>(string key, Func<GameObject, T> instanceProvider) where T : notnull, Component {
-            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefab => {
-                return InstantiateOperation<T>( prefab.Result, instanceProvider );
+            return Addressables.LoadAssetAsync<GameObject>( key ).ChainOperation( prefabsHandle => {
+                return InstantiateOperation<T>( prefabsHandle.Result, instanceProvider );
             } );
-        }
-
-        // Wait
-        public static void Wait<T>(this AsyncOperationHandle<T> handle) {
-            if (!handle.IsFailed()) {
-                handle.WaitForCompletion();
-                if (handle.IsSucceeded()) {
-                    return;
-                }
-            }
-            throw handle.OperationException;
-        }
-        public static async ValueTask WaitAsync<T>(this AsyncOperationHandle<T> handle, CancellationToken cancellationToken) {
-            if (!handle.IsFailed()) {
-                await handle.Task.WaitAsync( cancellationToken );
-                cancellationToken.ThrowIfCancellationRequested();
-                if (handle.IsSucceeded()) {
-                    return;
-                }
-            }
-            throw handle.OperationException;
-        }
-
-        // GetResult
-        public static T GetResult<T>(this AsyncOperationHandle<T> handle) {
-            if (!handle.IsFailed()) {
-                var result = handle.WaitForCompletion();
-                if (handle.IsSucceeded()) {
-                    return result;
-                }
-            }
-            throw handle.OperationException;
-        }
-        public static async ValueTask<T> GetResultAsync<T>(this AsyncOperationHandle<T> handle, CancellationToken cancellationToken) {
-            if (!handle.IsFailed()) {
-                var result = await handle.Task.WaitAsync( cancellationToken );
-                cancellationToken.ThrowIfCancellationRequested();
-                if (handle.IsSucceeded()) {
-                    return result;
-                }
-            }
-            throw handle.OperationException;
         }
 
         // Helpers
