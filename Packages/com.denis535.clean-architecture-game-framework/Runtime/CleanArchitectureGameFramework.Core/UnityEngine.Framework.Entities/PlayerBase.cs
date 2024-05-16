3eca114317a0a05e948b8145c58d68ab44b718a1
Denis <dddenisss35@gmail.com>
2024-05-16 21:20:58 +0300

    Player

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework.Core/UnityEngine.Framework.Entities/PlayerBase.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/CleanArchitectureGameFramework.Core/UnityEngine.Framework.Entities/PlayerBase.cs
@@ -3,13 +3,33 @@ namespace UnityEngine.Framework.Entities {
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Threading;
     using UnityEngine;
 
     public abstract class PlayerBase : IDisposable {
 
+        protected CancellationTokenSource? disposeCancellationTokenSource;
+
+        // System
+        public bool IsDisposed { get; protected set; }
+        public CancellationToken DisposeCancellationToken {
+            get {
+                if (disposeCancellationTokenSource == null) {
+                    disposeCancellationTokenSource = new CancellationTokenSource();
+                    if (IsDisposed) disposeCancellationTokenSource.Cancel();
+                }
+                return disposeCancellationTokenSource.Token;
+            }
+        }
+
         // Constructor
-        public PlayerBase() { }
-        public abstract void Dispose();
+        public PlayerBase() {
+        }
+        public virtual void Dispose() {
+            Assert.Object.Message( $"Player {this} must be alive" ).Alive( !IsDisposed );
+            IsDisposed = true;
+            disposeCancellationTokenSource?.Cancel();
+        }
 
     }
 }
