6ae07085b7194ca827f77e1b9a6dda3e7aba7d50
Denis <dddenisss35@gmail.com>
2024-08-27 09:23:23 +0300

    Fix

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework/UnityEngine.Framework.01.UI/UIWidget/UIRootWidgetBase.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework/UnityEngine.Framework.01.UI/UIWidget/UIRootWidgetBase.cs
@@ -41,7 +41,7 @@ namespace UnityEngine.Framework.UI {
             }
         }
         protected static void OnCancel(NavigationCancelEvent evt) {
-            var widget = ((VisualElement) evt.target).GetAncestorsAndSelf().Where( i => i.enabledInHierarchy && i.IsDisplayedInHierarchy() ).FirstOrDefault( IsWidget );
+            var widget = ((VisualElement) evt.target).GetAncestorsAndSelf().Where( i => i.enabledInHierarchy && i.IsDisplayedInHierarchy() ).LastOrDefault( IsWidget );
             var button = widget?.Query<Button>().Where( i => i.enabledInHierarchy && i.IsDisplayedInHierarchy() ).Where( IsCancel ).First();
             if (button != null) {
                 Click( button );
