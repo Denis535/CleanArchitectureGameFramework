6022755ebd6231ee6d84c3413caa44edd3bca8f2
Denis <dddenisss35@gmail.com>
2024-11-05 10:47:30 +0200

    Readme

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework/UnityEngine.Framework.00/ProgramEditor.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework/UnityEngine.Framework.00/ProgramEditor.cs
@@ -40,15 +40,16 @@ namespace UnityEngine.Framework {
                     EditorGUILayout.LabelField( "Links", EditorStyles.boldLabel );
                     if (EditorGUILayout.LinkButton( "denis535.github.io" )) Application.OpenURL( "https://denis535.github.io" );
                     if (EditorGUILayout.LinkButton( "github.com (Unity Shooter Example)" )) Application.OpenURL( "https://github.com/Denis535/UnityShooterExample" );
-                    if (EditorGUILayout.LinkButton( "github.com (Clean Architecture Game Framework)" )) Application.OpenURL( "https://github.com/Denis535/CleanArchitectureGameFramework" );
-                    EditorGUILayout.Space( 2f );
-                    if (EditorGUILayout.LinkButton( "youtube.com" )) Application.OpenURL( "https://www.youtube.com/channel/UCLFdZl0pFkCkHpDWmodBUFg" );
+                    if (EditorGUILayout.LinkButton( "github.com (Unity Framework)" )) Application.OpenURL( "https://github.com/Denis535/UnityFramework" );
                     EditorGUILayout.Space( 2f );
                     if (EditorGUILayout.LinkButton( "nuget.org" )) Application.OpenURL( "https://www.nuget.org/profiles/Denis535" );
                     if (EditorGUILayout.LinkButton( "openupm.com" )) Application.OpenURL( "https://openupm.com/packages/?sort=downloads&q=denis535" );
                     EditorGUILayout.Space( 2f );
+                    if (EditorGUILayout.LinkButton( "fab.com" )) Application.OpenURL( "https://www.fab.com/sellers/Denis535" );
                     if (EditorGUILayout.LinkButton( "assetstore.unity.com" )) Application.OpenURL( "https://assetstore.unity.com/publishers/90787" );
-                    if (EditorGUILayout.LinkButton( "itch.io" )) Application.OpenURL( "https://denis535.itch.io/" );
+                    EditorGUILayout.Space( 2f );
+                    if (EditorGUILayout.LinkButton( "youtube.com" )) Application.OpenURL( "https://www.youtube.com/channel/UCLFdZl0pFkCkHpDWmodBUFg" );
+                    if (EditorGUILayout.LinkButton( "udemy.com" )) Application.OpenURL( "https://www.udemy.com/user/denis-84102" );
                 }
                 EditorGUILayout.Separator();
                 {
--- Packages/com.denis535.colorful-project-window/Readme.md
+++ Packages/com.denis535.colorful-project-window/Readme.md
@@ -40,9 +40,10 @@ You can set up the colors in the 'Preferences/Colorful Project Window' window.
 - https://denis535.github.io
 - https://www.nuget.org/profiles/Denis535
 - https://openupm.com/packages/?sort=downloads&q=denis535
-- https://assetstore.unity.com/publishers/90787
 - https://www.fab.com/sellers/Denis535
+- https://assetstore.unity.com/publishers/90787
 - https://www.youtube.com/channel/UCLFdZl0pFkCkHpDWmodBUFg
+- https://www.udemy.com/user/denis-84102
 
 # If you want to support me
 If you want to support me, please rate my packages, subscribe to my YouTube channel and like my videos.
