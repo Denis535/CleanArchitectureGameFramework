03e37ff07cd3070552af8e24d0aa13a291745199
Denis <dddenisss35@gmail.com>
2024-11-14 22:49:10 +0200

    Option

--- Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework.Internal/System/Option.Pro/Option.cs
+++ Packages/com.denis535.clean-architecture-game-framework/Runtime/Denis535.CleanArchitectureGameFramework.Internal/System/Option.Pro/Option.cs
@@ -4,88 +4,99 @@ namespace System {
     using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
 
     public static class Option {
 
-        // Equals
-        public static bool Equals<T>(Option<T> v1, Option<T> v2) {
-            if (v1.HasValue && v2.HasValue) return EqualityComparer<T>.Default.Equals( v1.Value, v2.Value );
-            return EqualityComparer<bool>.Default.Equals( v1.HasValue, v2.HasValue );
+        // Create
+        public static Option<T> Create<T>(T value) {
+            return new Option<T>( value );
         }
-        public static bool Equals<T>(Option<T> v1, T v2) {
-            if (v1.HasValue) return EqualityComparer<T>.Default.Equals( v1.Value, v2 );
+        public static Option<T> Create<T>(T? value) where T : struct {
+            if (value.HasValue) return new Option<T>( value.Value );
+            return default;
+        }
+
+        // Equals
+        public static bool Equals<T>(Option<T> value, Option<T> other) {
+            if (value.HasValue && other.HasValue) return EqualityComparer<T>.Default.Equals( value.Value, other.Value );
+            return EqualityComparer<bool>.Default.Equals( value.HasValue, other.HasValue );
+        }
+        public static bool Equals<T>(Option<T> value, T other) {
+            if (value.HasValue) return EqualityComparer<T>.Default.Equals( value.Value, other );
             return false;
         }
-        public static bool Equals<T>(T v1, Option<T> v2) {
-            if (v2.HasValue) return EqualityComparer<T>.Default.Equals( v1, v2.Value );
+        public static bool Equals<T>(T value, Option<T> other) {
+            if (other.HasValue) return EqualityComparer<T>.Default.Equals( value, other.Value );
             return false;
         }
 
         // Compare
-        public static int Compare<T>(Option<T> v1, Option<T> v2) {
-            if (v1.HasValue && v2.HasValue) return Comparer<T>.Default.Compare( v1.Value, v2.Value );
-            return Comparer<bool>.Default.Compare( v1.HasValue, v2.HasValue );
+        public static int Compare<T>(Option<T> value, Option<T> other) {
+            if (value.HasValue && other.HasValue) return Comparer<T>.Default.Compare( value.Value, other.Value );
+            return Comparer<bool>.Default.Compare( value.HasValue, other.HasValue );
         }
-        public static int Compare<T>(Option<T> v1, T v2) {
-            if (v1.HasValue) return Comparer<T>.Default.Compare( v1.Value, v2 );
+        public static int Compare<T>(Option<T> value, T other) {
+            if (value.HasValue) return Comparer<T>.Default.Compare( value.Value, other );
             return Comparer<bool>.Default.Compare( false, true );
         }
-        public static int Compare<T>(T v1, Option<T> v2) {
-            if (v2.HasValue) return Comparer<T>.Default.Compare( v1, v2.Value );
+        public static int Compare<T>(T value, Option<T> other) {
+            if (other.HasValue) return Comparer<T>.Default.Compare( value, other.Value );
             return Comparer<bool>.Default.Compare( true, false );
         }
 
         // GetUnderlyingType
-        public static Type? GetUnderlyingType(Type type) {
-            if (GetUnboundType( type ) == typeof( Option<> )) return type.GetGenericArguments().First();
-            return null;
-        }
-
-        // Helpers
-        private static Type GetUnboundType(Type type) {
-            if (type.IsGenericType) {
-                return type.IsGenericTypeDefinition ? type : type.GetGenericTypeDefinition();
-            } else {
-                return type;
-            }
+        public static Type GetUnderlyingType(Type type) {
+            if (type == null) throw new ArgumentNullException( "type" );
+            if (type.IsGenericType && !type.IsGenericTypeDefinition && type.GetGenericTypeDefinition() != typeof( Option<> )) throw new ArgumentException( "Argument 'type' is invalid" );
+            return type.GetGenericArguments()[ 0 ];
         }
 
     }
-    // Option
     [Serializable]
     public readonly struct Option<T> : IEquatable<Option<T>>, IEquatable<T>, IComparable<Option<T>>, IComparable<T> {
 
         private readonly bool hasValue;
-        private readonly T value; // Note: may be null/default if Option has no value
+        private readonly T value; // always is null/default when Option has no value
 
         // Value
-        public bool HasValue => hasValue; // Note: Option can have null/default value
-        public T Value => hasValue ? value : throw new InvalidOperationException( "Option has no value" ); // Note: therefore, null/default is also valid Option's value
-        public T? ValueOrDefault => hasValue ? value : default; // always null/default if Option has no value
+        public bool HasValue => hasValue;
+        public T Value => hasValue ? value : throw new InvalidOperationException( "Option has no value" );
+        public T? ValueOrDefault => hasValue ? value : default; // always is null/default when Option has no value
 
         // Constructor
-        //public Option() { // C# 9.0 doesn't support it
+        //public Option() {
         //    this.hasValue = false;
         //    this.value = default!;
         //}
-        public Option(T value) {
+        internal Option(T value) {
             this.hasValue = true;
             this.value = value;
-        }
-        private Option(bool hasValue, T? value) {
-            this.hasValue = hasValue;
-            this.value = value!;
         }
 
         // TryGetValue
         public bool TryGetValue([MaybeNullWhen( false )] out T value) {
             if (HasValue) {
-                value = this.value;
+                value = this.Value;
                 return true;
             }
             value = default;
             return false;
+        }
+
+        // Equals
+        public bool Equals(Option<T> other) {
+            return Option.Equals( this, other );
+        }
+        public bool Equals(T other) {
+            return Option.Equals( this, other );
+        }
+
+        // CompareTo
+        public int CompareTo(Option<T> other) {
+            return Option.Compare( this, other );
+        }
+        public int CompareTo(T other) {
+            return Option.Compare( this, other );
         }
 
         // Utils
@@ -103,32 +114,19 @@ namespace System {
         }
 
         // Utils
-        public bool Equals(Option<T> other) {
-            return Option.Equals( this, other );
-        }
-        public bool Equals(T other) {
-            return Option.Equals( this, other );
-        }
-
-        // Utils
-        public int CompareTo(Option<T> other) {
-            return Option.Compare( this, other );
-        }
-        public int CompareTo(T other) {
-            return Option.Compare( this, other );
-        }
-
-        // Utils
-        public static explicit operator Option<object?>(Option<T> value) {
-            // todo: how to cast any generic option to any other generic option?
-            // https://github.com/dotnet/csharplang/issues/813
-            return new Option<object?>( value.hasValue, value.value );
-        }
-        public static explicit operator Option<T>(Option<object?> value) {
-            // todo: how to cast any generic option to any other generic option?
-            // https://github.com/dotnet/csharplang/issues/813
-            return new Option<T>( value.hasValue, (T?) value.value );
-        }
+        //public static explicit operator Option<T>(T value) {
+        //    return new Option<T>( value );
+        //}
+        //public static explicit operator Option<object?>(Option<T> value) {
+        //    // Todo:    
+        //    // How to write explicit cast operator with its own generic type???
+        //    // Now I can cast only to non-generic types!!!
+        //    // https://github.com/dotnet/csharplang/issues/813
+        //    return new Option<object?>( value.hasValue, value.value );
+        //}
+        //public static explicit operator Option<T>(Option<object?> value) {
+        //    return new Option<T>( value.hasValue, (T) value.value );
+        //}
 
         // Utils
         public static bool operator ==(Option<T> left, Option<T> right) {
@@ -150,19 +148,6 @@ namespace System {
         }
         public static bool operator !=(T left, Option<T> right) {
             return !Option.Equals( left, right );
-        }
-
-    }
-    // OptionExtensions
-    public static class OptionExtensions {
-
-        // AsOption
-        public static Option<T> AsOption<T>(this T value) {
-            return new Option<T>( value );
-        }
-        public static Option<T> AsOption<T>(this T? value) where T : struct {
-            if (value.HasValue) return new Option<T>( value.Value );
-            return default;
         }
 
     }
